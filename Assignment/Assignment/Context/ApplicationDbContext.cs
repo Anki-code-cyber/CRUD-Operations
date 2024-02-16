be848df13008f8d3879ed7070214ddb895a14050
rahul0412 <rahul.jha@mymobiforce.com>
2024-02-17 00:17:56 +0530

    Final Changes

---
+++ Assignment/Assignment/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Assignment.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Assignment.Context
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+            : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<Setting> Setting { get; set; }
+    }
+}
---
+++ Assignment/Assignment/Models/Setting.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment.Models
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Value2 { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
